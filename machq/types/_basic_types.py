caf0e2a1120cfb26ec362d988f49757c2e01bd99
brendan-reid91 <brendan.reid1991@gmail.com>
2022-11-14 11:14:02 +0000

    Added NewType and moved Circuit

---
+++ machq/types/_basic_types.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+from typing import List, Tuple, Optional, NamedTuple, NewType
+import stim
+
+from machq.noise import NoiseProfile, NoiseChannels, DepolarizingNoise
+
+
+Distance = NewType("Distance", int)
+
+
+class Qubit(NamedTuple):
+    """A class that allows easy reference to
+    qubit coordinates.
+    """
+
+    x: int
+    y: int
+
+    def __add__(self, other):
+        if isinstance(other, tuple) or isinstance(other, list):
+            other = Qubit(*other)
+        if isinstance(other, Qubit):
+            return Qubit(self.x + other.x, self.y + other.y)
+        raise NotImplementedError()
+
+    def __mul__(self, other):
+        if isinstance(other, int):
+            return Qubit(self.x * other, self.y * other)
+        raise NotImplementedError()
+
+    def __eq__(self, other):
+        if isinstance(other, Qubit):
+            return self.x == other.x and self.y == other.y
--- machq/types/_circuit_.py
+++ machq/types/_circuit_.py
@@ -1,50 +1,8 @@
 from __future__ import annotations
-from typing import List, Tuple, Optional, NamedTuple
+from typing import List, Tuple, Optional
 import stim
 
 from machq.noise import NoiseProfile, NoiseChannels, DepolarizingNoise
-
-ONE_QUBIT_GATES = [
-    "H",
-    "S",
-    "S_DAG",
-    "X",
-    "SQRT_X",
-    "SQRT_X_DAG",
-    "Y",
-    "SQRT_Y",
-    "SQRT_Y_DAG",
-    "Z",
-]
-
-TWO_QUBIT_GATES = [
-    "CX",
-]
-
-
-class Qubit(NamedTuple):
-    """A class that allows easy reference to
-    qubit coordinates.
-    """
-
-    x: int
-    y: int
-
-    def __add__(self, other):
-        if isinstance(other, tuple) or isinstance(other, list):
-            other = Qubit(*other)
-        if isinstance(other, Qubit):
-            return Qubit(self.x + other.x, self.y + other.y)
-        raise NotImplementedError()
-
-    def __mul__(self, other):
-        if isinstance(other, int):
-            return Qubit(self.x * other, self.y * other)
-        raise NotImplementedError()
-
-    def __eq__(self, other):
-        if isinstance(other, Qubit):
-            return self.x == other.x and self.y == other.y
 
 
 class Circuit:
