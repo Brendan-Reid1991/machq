e7b26719088873e985a7a52464630d2853c90750
brendan-reid91 <brendan.reid1991@gmail.com>
2022-11-16 09:29:41 +0000

    improved type checking in qmem

--- machq/analysis/quantum_memory.py
+++ machq/analysis/quantum_memory.py
@@ -1,3 +1,5 @@
+from typing import Type
+
 from machq.codes import QuantumErrorCorrectionCode
 from machq.decoders import Decoder
 
@@ -5,7 +7,7 @@ from machq.decoders import Decoder
 class QuantumMemory:
     """A class that allows you to run quantum memory experiments."""
 
-    def __init__(self, code: QuantumErrorCorrectionCode, decoder: Decoder):
+    def __init__(self, code: QuantumErrorCorrectionCode, decoder: Type[Decoder]):
         self.code = code
         self.decoder = decoder(circuit=self.code.circuit)
 
@@ -37,4 +39,18 @@ class QuantumMemory:
             rounds = rounds if rounds is not None else self.code.z_distance
             self.code.logical_z_memory(rounds=rounds)
 
-        return self.decoder.logical_failure_probability(num_shots=num_shots)
+        return self.decoder.logical_failure_probability(num_shots)
+
+
+if __name__ == "__main__":
+    from machq.codes import RotatedPlanarCode
+    from machq.noise import DepolarizingNoise
+    from machq.decoders import PyMatching
+
+    for d in [3, 5, 7, 9]:
+        code = RotatedPlanarCode(
+            x_distance=d, z_distance=d, noise_profile=DepolarizingNoise(p=0.001)
+        )
+
+        qmem = QuantumMemory(code=code, decoder=PyMatching)
+        print(qmem.memory_experiment(logical="Z", num_shots=1e5))
