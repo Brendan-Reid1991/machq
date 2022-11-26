6462ba45034ffe3ffc9a1e69c07afeae412cf5c5
brendan-reid91 <brendan.reid1991@gmail.com>
2022-11-26 11:22:15 +0000

    Removed gadget

--- machq/codes/__init__.py
+++ machq/codes/__init__.py
@@ -1,3 +1,2 @@
 from machq.codes._qec_code import QuantumErrorCorrectionCode
 from machq.codes.rotated_planar_code import RotatedPlanarCode
-from machq.codes.rotated_planar_code_measurement_gadget import RotatedPlanarCodeGadget
